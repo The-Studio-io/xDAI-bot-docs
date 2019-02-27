8c3d815eef8a55a093e64a727fb5479332572cb3
Yashu Mittal <mittalyashu77@gmail.com>
2019-02-28 00:55:38 +0530

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,16 @@
 # abort on errors
 set -e
 
-# delete old dist directory
-rm -rf dist
-
 # build
 vuepress build
 
 # navigate into the build output directory
 cd dist
 
-# commit the build files
-git add .
+git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push git@github.com:The-Studio-io/xDAI-bot-docs.git master:gh-pages
+git push -f origin master:gh-pages
 
 cd -
